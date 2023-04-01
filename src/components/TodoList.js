04f37ed1686881d7376f8cdf5fdf651133a2b317
sauhardLohan <57857933+sauhardLohan@users.noreply.github.com>
2023-04-01 23:20:07 +0530

    removing warning

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,4 @@
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 function TodoList(props) {
   const { list, handleUpdate, handleDelete, handleEdit } = props;
